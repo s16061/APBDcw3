05dc15cb67e8bd4f6baf9aef52487104ae258f85
Michał Karczmarczyk <s16061@pjwstk.edu.pl>
2021-01-19 14:09:48 +0100

    APBDcw3

---
+++ Cw3/Models/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw3.Models
+{
+    public class Students
+    {
+        public int IdStudent { get; set; }
+        public String FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IndexNumber { get; set; }
+    }
+}
